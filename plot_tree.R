8644d00e1caa7e670d9664875719aff9485082be
Adam Orr <ajorr1@asu.edu>
2016-04-05 14:40:39 -0700

    finish plot_tree, increase text size

--- plot_tree.R
+++ plot_tree.R
@@ -1,17 +1,20 @@
 #!usr/bin/env Rscript
 #Rscript plot_tree.R tree.nwk out_file.pdf
 
-library(ape)
-library(phytools)
+library('ape')
+library('phytools')
 mycolors <- c('lightgreen','yellow','purple','red')
 
 args <- commandArgs(trailingOnly = TRUE)
 tree <- read.tree(args[1])
 pdf(args[2],8,8)
-plot(tree, show.tip.label = FALSE, no.margin = TRUE, y.lim = c(0,9))
-tree$tip.label = 
-tiplabels(tree$tip.label,frame="none")
 
+#Fix labels
 tree$tip.label = unlist(lapply(tree$tip.label, FUN=function(x) strsplit(x,'_')[[1]][1]))
 tree$tip.label = gsub("M2c","M3a",tree$tip.label) #fix an oopsie
-tree$tip.label = grep
+m <- regexpr("[0-9]+",tree$tip.label)
+tree$tip.label = regmatches(tree$tip.label,m)
+
+plot(tree, no.margin = TRUE, cex = 2)
+
+q()
