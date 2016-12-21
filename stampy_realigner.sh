8bed55176852df736c3b1295f7f9df61a3408bcd
Adam Orr <ajorr1@asu.edu>
2016-12-20 18:20:12 -0700

    get rid of error messages when there are no subprocesses to kill

--- stampy_realigner.sh
+++ stampy_realigner.sh
@@ -77,7 +77,7 @@ FIXEDINFILE=$(mktemp --tmpdir=$TMPDIR --suffix=.bam fixed_in_XXX)
 MAPPEDREADS=$(mktemp --tmpdir=$TMPDIR --suffix=.bam mapped_XXX)
 UNMAPPEDREADS=$(mktemp --tmpdir=$TMPDIR --suffix=.bam unmapped_XXX)
 
-trap 'kill $(jobs -pr); rm -rf $TMPDIR' EXIT INT TERM HUP
+trap '[ -n "$(jobs -pr)" ] && kill $(jobs -pr); rm -rf $TMPDIR' EXIT INT TERM HUP
 trap "exit 1" ERR
 
 
