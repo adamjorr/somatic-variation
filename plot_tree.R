edc46c1157952e18138d0f41b2eeb36d97794b6e
Adam Orr <ajorr1@asu.edu>
2016-12-21 17:34:48 -0700

    no need for substitution since we call the file properly now

--- plot_tree.R
+++ plot_tree.R
@@ -11,7 +11,6 @@ pdf(args[2],8,8)
 
 #Fix labels
 tree$tip.label = unlist(lapply(tree$tip.label, FUN=function(x) strsplit(x,'_')[[1]][1]))
-tree$tip.label = gsub("M2c","M3a",tree$tip.label) #fix an oopsie
 m <- regexpr("[0-9]+",tree$tip.label)
 tree$tip.label = regmatches(tree$tip.label,m)
 
