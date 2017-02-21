97b8bed6bbc1252b2203c7e8a2268f108137dc69
Adam Orr <adamjorr@gmail.com>
2017-02-21 13:32:18 -0700

    fix broken tmp directory

--- create_consensus.sh
+++ create_consensus.sh
@@ -80,7 +80,7 @@ if [ "$REFFILE" == "" ]; then
 	exit 1
 fi
 
-TMPDIR=$(mktemp -d --tmpdir=$TMPOPTION $0_tmp_XXXXXX)
+TMPDIR=$(mktemp -d --tmpdir=$TMPOPTION $(basename $0)_tmp_XXXXXX)
 trap "rm -rf $TMPDIR" EXIT INT TERM HUP
 trap "exit 1" ERR
 
