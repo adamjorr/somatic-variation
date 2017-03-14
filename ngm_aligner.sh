e0b176576247ac0a6ef243819575ad2661e81d6e
Adam Orr <adamjorr@gmail.com>
2017-03-14 16:11:47 -0700

    add ngm_aligner.sh

---
+++ ngm_aligner.sh
@@ -0,0 +1,131 @@
+#!/usr/bin/bash
+
+#bwa_aligner.sh reference.fasta
+#Takes reference and aligns all .fastq files in any subdirectory.
+#This is a modified version of align_fastq which uses bwa instead of bowtie.
+USAGE="Usage: $0 [-t THREADS] [-d TMPDIR] [-p RG_PLATFORM] [-q FILEPATTERN] [-1 FIRSTMATE] [-2 SECONDMATE] [-o out.bam] -r reference.fa -i data/"
+
+
+
+
+#Here are some things you might want to change:
+CORES=48
+FILE_PATTERN='*.fastq' #pattern to find first set of reads
+SEARCH_STRING='R1' #pattern for search/replace to find second set of reads
+REPLACE_STRING='R2' #pattern for search/replace to substitute second set of reads
+REFERENCEFILE=""
+TMPOPTION=""
+DATADIR=""
+OUTNAME=/dev/stdout
+
+while getopts :t:d:p:r:1:2:o:i:q:h opt; do
+	case $opt in
+		t)
+			CORES=$OPTARG
+			;;
+		d)
+			TMPOPTION=$OPTARG
+			;;
+		p)
+			RGPL=$OPTARG
+			;;
+		r)
+			REFERENCEFILE=$OPTARG
+			;;
+		1)
+			SEARCH_STRING=$OPTARG
+			;;
+		2)
+			REPLACE_STRING=$OPTARG
+			;;
+		o)
+			OUTNAME=$OPTARG
+			;;
+		i)
+			DATADIR=$OPTARG
+			;;
+		q)
+			FILE_PATTERN=$OPTARG
+			;;
+		h)
+			echo $USAGE >&2
+			exit 1
+			;;
+	    \?)
+			echo "Invalid option: -$OPTARG" >&2
+			exit 1
+			;;
+		:)
+			echo "Option -$OPTARG requires an argument." >&2
+			exit 1
+			;;
+	esac
+done
+
+shift $((OPTIND-1)) # get operands
+if [ $# -ne 0 ]; then			#if we forget arguments
+	echo $USAGE	>&2#remind us
+	exit 1				#and exit with error
+fi
+
+if [ "$REFERENCEFILE" == "" ]; then
+	echo $USAGE >&2
+	echo "Reference file required." >&2
+	exit 1
+fi
+
+if [ "$DATADIR" == "" ]; then
+	echo $USAGE >&2
+	echo "Input directory required." >&2
+	exit 1
+fi
+
+if [ ! -d "$DATADIR" ]; then
+	echo $USAGE >&2
+	echo "$DATADIR is not a directory." >&2
+	exit 1
+fi
+
+if [ $CORES -lt 2 ]; then
+	echo $USAGE >&2
+	echo "Specify two or more threads." >&2
+	exit 1
+fi
+
+TMPDIR=$(mktemp -d --tmpdir=$TMPOPTION $(basename $0)_tmp_XXXXXX)
+trap "rm -rf $TMPDIR" EXIT INT TERM HUP
+trap "exit 1" ERR
+
+
+#Some variables
+FASTQFILES=$(find $DATADIR -name "$FILE_PATTERN" -and -name "*${SEARCH_STRING}*")
+
+if [ "$FASTQFILES" == '' ]; then
+	echo "Searching for files that match $FILE_PATTERN and ${SEARCH_STRING} in $DATADIR failed" >&2
+	exit 1
+fi
+
+echo Specified files are: $FASTQFILES >&2
+echo Specified mates are: ${FASTQFILES/$SEARCH_STRING/$REPLACE_STRING} >&2
+NUMFILES=( $FASTQFILES )
+NUMFILES=${#NUMFILES[@]}
+
+echo Making BAM files . . . >&2
+#Make bamfiles from the FASTQs
+PROGRESS=1
+for F in $FASTQFILES; do
+	echo Progress: $PROGRESS / $NUMFILES >&2
+	BASEFNAME=$(basename $F)
+	SECONDMATE=${F/$SEARCH_STRING/$REPLACE_STRING}
+	SECONDBASE=$(basename $SECONDMATE)
+	BAMOUT=$(mktemp --tmpdir=$TMPDIR --suffix=.bam ${BASEFNAME%$SEARCH_STRING*}_XXXXXXXXXX)
+	BAMS=$(echo $BAMS $BAMOUT)
+	ngm -t $(( CORES/2 )) -r $REFERENCEFILE -p -1 $F -2 $SECONDMATE | 
+		samtools sort -@ $(( CORES/2 )) -o $BAMOUT -O bam -m 2G -T ${TMPDIR}/
+	((PROGRESS++))
+done
+
+echo Merging . . . >&2
+samtools merge -@ $CORES $OUTNAME $BAMS
+
+exit 0
