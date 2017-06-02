3a1c0edabf2be4166d31b2e357c536c70a5652a6
David Kainer <dxk800@raijin6.(none)>
2017-06-02 17:41:56 +1000

    new NCI version. Does not merge all output BAMs into one

--- ngm_aligner.sh
+++ ngm_aligner.sh
@@ -3,15 +3,13 @@
 #bwa_aligner.sh reference.fasta
 #Takes reference and aligns all .fastq files in any subdirectory.
 #This is a modified version of align_fastq which uses bwa instead of bowtie.
-USAGE="Usage: $0 [-t THREADS] [-d TMPDIR] [-p RG_PLATFORM] [-q FILEPATTERN] [-1 FIRSTMATE] [-2 SECONDMATE] [-o out.bam] -r reference.fa -i data/"
-
+USAGE="Usage: $0 [-t THREADS] [-d TMPDIR] [-p RG_PLATFORM] [-q FILEPATTERN] [-1 FIRSTMATE] [-2 SECONDMATE] [-o outdir] -r reference.fa -i data/"
 
 
 
 #Here are some things you might want to change:
 RGPL=ILLUMINA #Default platform
-CORES=48
-FILE_PATTERN='*.fastq' #pattern to find first set of reads
+FILE_PATTERN='*.fastq.gz' #pattern to find first set of reads
 SEARCH_STRING='R1' #pattern for search/replace to find second set of reads
 REPLACE_STRING='R2' #pattern for search/replace to substitute second set of reads
 REFERENCEFILE=""
@@ -41,7 +39,7 @@ while getopts :t:d:p:r:1:2:o:i:q:s:h opt; do
 			REPLACE_STRING=$OPTARG
 			;;
 		o)
-			OUTNAME=$OPTARG
+			OUTDIR=$OPTARG
 			;;
 		i)
 			DATADIR=$OPTARG
@@ -91,16 +89,6 @@ if [ ! -d "$DATADIR" ]; then
 	exit 1
 fi
 
-if [ $CORES -lt 2 ]; then
-	echo $USAGE >&2
-	echo "Specify two or more threads." >&2
-	exit 1
-fi
-
-TMPDIR=$(mktemp -d --tmpdir=$TMPOPTION $(basename $0)_tmp_XXXXXX)
-trap "rm -rf $TMPDIR" EXIT INT TERM HUP
-trap "exit 1" ERR
-
 
 #Some variables
 FASTQFILES=$(find $DATADIR -name "$FILE_PATTERN" -and -name "*${SEARCH_STRING}*")
@@ -127,19 +115,13 @@ for F in $FASTQFILES; do
 	BASEFNAME=$(basename $F)
 	SECONDMATE=${F/$SEARCH_STRING/$REPLACE_STRING}
 	SECONDBASE=$(basename $SECONDMATE)
-	TMPSAM=$(mktemp --tmpdir=$TMPDIR --suffix=.sam ${BASEFNAME%$SEARCH_STRING*}_XXXXXXXXXX)
-	BAMOUT=$(mktemp --tmpdir=$TMPDIR --suffix=.bam ${BASEFNAME%$SEARCH_STRING*}_XXXXXXXXXX)
-	BAMS=$(echo $BAMS $BAMOUT)
-	RGPU=$(head -n 1 $F | cut -d: -f3,4 --output-delimiter=.)
-	RGLB=$(expr $F : '.*\(M[0-9]*[abc]\)') || RGLB=$F
-	RGSM=$(expr $F : '.*\(M[0-9]*[abc]\)') || RGSM=$F
-	ngm -t ${CORES} $SENSITIVITY -r $REFERENCEFILE -p -1 $F -2 $SECONDMATE --rg-id ${RGSM} --rg-sm ${RGSM} --rg-lb ${RGLB} --rg-pl ${RGPL} --rg-pu ${RGPU} -o $TMPSAM
-	samtools sort -@ ${CORES} -o $BAMOUT -O bam -m 2G -T ${TMPDIR}/ $TMPSAM
-	rm $TMPSAM
+
+	RGPU=$(zcat $F | head -n 1 | cut -d: -f3,4 --output-delimiter=.)
+	RGLB=$(expr $F : '.*\(RL[0-9][0-9]\)') || RGLB=$F
+	RGSM=$(expr $F : '.*\(RL[0-9][0-9]\)') || RGSM=$F
+
+	qsub -v REF=$REFERENCEFILE,R1=$F,R2=$SECONDMATE,RGSM=$RGSM,RGLB=$RGLB,RGPU=$RGPU,BAMDIR=$OUTDIR HPC_jobs/ngm.job
 	((PROGRESS++))
 done
-
-echo Merging . . . >&2
-samtools merge -c -p -@ $CORES $OUTNAME $BAMS
 
 exit 0
