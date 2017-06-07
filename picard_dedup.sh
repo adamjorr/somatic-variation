6965258fea46c30cc231b961d2e3d751bd8d6d1f
David Kainer <dxk800@raijin4.(none)>
2017-06-08 09:59:18 +1000

    added HPC jobs. Clauster Parallelised ngm_align.sh and the GATK caller

--- picard_dedup.sh
+++ picard_dedup.sh
@@ -6,19 +6,13 @@
 #requires samtools, picard-tools, the GATK, and bowtie2
 
 #http://tldp.org/LDP/abs/html/string-manipulation.html is a great guide for manipulating strings in bash
-USAGE="Usage: $0 [-p PICARD_CMD] [-d TMPDIR] [-o OUTDIR] -i bamdir"
+USAGE="Usage: $0 -i bamdir"
 
 #Here are some things you might want to change:
 PICARD="java -jar ~/apps/bin/picard.jar" #How do I call picard on this system? on NCI make sure you load the Java module first
 
-while getopts :p:o:r:i:h opt; do
+while getopts :o:i:h opt; do
         case $opt in
-                p)
-                        PICARD=$OPTARG
-                        ;;
-                o)
-                        OUTDIR=$OPTARG
-                        ;;
                 i)
                         INDIR=$OPTARG
                         ;;
@@ -38,8 +32,7 @@ while getopts :p:o:r:i:h opt; do
         esac
 done
 
-
-if [ $# -ne 0 ]; then                   #if we forget arguments
+if [ $# -eq 0 ]; then                   #if we forget arguments
         echo $USAGE >&2 #remind us
         exit 1                          #and exit with error
 fi
@@ -51,15 +44,21 @@ if [ "INDIR" == "" ]; then
 fi
 
 
-mkdir -p $INDIR/dedup
-DEDUPDIR=$INDIR/dedup
+DEDUPDIR=${INDIR}/dedup
+MERGEDDIR=${DEDUPDIR}/merge
+
+mkdir -m777 -p $DEDUPDIR
+mkdir -m777 -p $MERGEDDIR
 
 # submit each BAM deduplication job to a cluster node
+JOBIDLIST=""
 for BAM in $(find $INDIR/*.bam); do
-        DEDUPLIFIEDBAM=$(mktemp --tmpdir=$TMPDIR --suffix=.bam dedup_XXX)
         echo "qsub -v BAM=${BAM},OUTDIR=${DEDUPDIR} picard_dedup.job"
-        qsub -v BAM=${BAM},OUTDIR=${DEDUPDIR} HPC_jobs/picard_dedup.job
-        sleep 1
+        JOBIDLIST=${JOBIDLIST}:$(qsub -v BAM=${BAM},OUTDIR=$DEDUPDIR HPC_jobs/picard_dedup.job)		# submit a dedup job and record the job id in a string
+#        sleep 1
 done
+
+# detect that all dedup jobs have completed successfully, then merge all the samples' BAMs into one, then index the big bam
+qsub -W depend=afterok${JOBIDLIST} -v DEDUPDIR=${DEDUPDIR},MERGEDDIR=$MERGEDDIR HPC_jobs/bam_merge_index.job
 
 exit 0
