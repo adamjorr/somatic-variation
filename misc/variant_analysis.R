ea29b3769ad6ab97c30f611900727e1140f86a39
Adam Orr <adamjorr@gmail.com>
2018-04-23 18:07:55 -0700

    update variant table generating script

--- misc/variant_analysis.R
+++ misc/variant_analysis.R
@@ -1,96 +1,60 @@
+#!/usr/bin/env Rscript
 library(VariantAnnotation)
 library(IRanges)
 library(GenomicRanges)
 library(GenomicFeatures)
 library(tidyverse)
 
+args <- commandArgs(trailingOnly = TRUE)
 
-# genome <- toGRanges("chromosomes.txt")
-# bands <- toGRanges("genes.txt")
-# withrepeats <- read_tsv("positions_with_repeats.txt", col_names=c("chr","pos"))
-# norepeats <- read_tsv("positions_no_repeats.txt", col_names=c("chr","pos"))
-# genes <- read_tsv("genes.txt") %>%
-#   group_by(chr)
-# 
-# in_range_ <- function(chrm, pos, genes){
-#   rnges <- genes %>% filter(chr == chrm)
-#   btwn <- function(pos, start, end){between(pos,start,end)}
-#   result <- map2(rnges$start,rnges$end, ~ btwn(pos,.x,.y))
-#   return(TRUE %in% result)
-# }
-# in_range <- function(chrm,pos,genes){
-#   return(map2(chrm,pos,~ in_range_(.x,.y,genes)))
-# }
-# 
-# variant_table <- norepeats %>%
-#   mutate(repeatregion = FALSE) %>%
-#   right_join(withrepeats) %>%
-#   mutate(repeatregion = is.na(.$repeatregion)) %>%
-#   mutate(ingene = unlist(in_range(chr,pos,genes)))
+#file names
+vcffile <- args[1]
+fastafile <- args[2]
+gfffile <- args[3]
+repeatsgfffile <- args[4]
 
-vcffile <- "passed.vcf"
-fastafile <- "e_mel_3.fa"
-gfffile <- "e_mel_3_genes.gff3"
-repeatsgfffile <- "e_mel_3_repeats.gff3"
-# this may break if tidyverse is loaded??
-# vcf <- readVcf(vcffile,fastafile)
+#loaded files
+vcf <- readVcf(vcffile,fastafile)
 gff <- GenomicFeatures::makeTxDbFromGFF(gfffile)
+repeatgff <- import.gff3(repeatsgfffile)
 locs <- locateVariants(vcf,gff,AllVariants())
 fa <- FaFile(fastafile)
-indexFa(fastafile)
 coding <- predictCoding(vcf,gff,seqSource=fa)
 
-#overlapsAny(locs, genes(gff)) seems to do what i want
-
-
-# load tidy things here
-positions <- as_tibble(start(ranges(coding)))
-names(positions) <- "pos"
-chrs <- as_tibble(seqnames(coding))
-names(chrs) <- "chr"
-coding <- chrs %>%
-  bind_cols(positions) %>%
-  bind_cols(as_tibble(mcols(coding)))
-
-positions <- as_tibble(start(ranges(locs)))
-names(positions) <- "pos"
-chrs <- as_tibble(seqnames(locs))
-names(chrs) <- "chr"
-
-locs <- chrs %>%
-  bind_cols(positions) %>%
-  bind_cols(as_tibble(mcols(locs)))
-
-type <- locs %>%
-  select(chr, pos, LOCATION) %>%
-  distinct()
-
-coding <- coding %>%
-  select(chr, pos, CONSEQUENCE, REFAA, VARAA)
-
-variant_table <- variant_table %>%
-  left_join(type,by = c("chr","pos")) %>%
-  left_join(coding,by = c("chr","pos")) 
-
-# variant_table <- variant_table %>%
-#   mutate(seq = unlist(map2(.$chr,.$pos, ~ as.character(getSeq(fa, GRanges(seqnames = .x, IRanges(start = .y - 1000, end = .y + 1000)))))))
-
+#tibbles of useful data
 gts <- as.tibble(geno(genotypeCodesToNucleotides(vcf))$GT)
 
-refalt <- as_tibble(mcols(rowRanges(vcf))) %>%
-  select(REF, ALT) %>%
-  mutate(ALT = unlist(map(.$ALT, ~ as.character(.)))) %>%
-  bind_cols(as_tibble(as.data.frame(seqnames(rowRanges(vcf))))) %>%
-  rename(chr = value) %>%
-  bind_cols(as_tibble(start(ranges(rowRanges(vcf))))) %>%
-  rename(pos = value) %>%
-  select(chr, pos, REF, ALT) %>%
-  bind_cols(gts) %>%
-  mutate(upstreamkb = unlist(map2(.$chr,.$pos, ~as.character(getSeq(fa,GRanges(seqnames = .x, IRanges(start = .y - 1000, end = .y - 1))))))) %>%
-  mutate(downstreamkb = unlist(map2(.$chr,.$pos, ~as.character(getSeq(fa,GRanges(seqnames = .x, IRanges(start = .y + 1 , end = .y + 1000))))))) %>%
-  select(chr:ALT, upstreamkb, M1a:M8c, downstreamkb)
+variant_table <- tibble(chr = as.character(seqnames(vcf)),
+                        pos = start(vcf),
+                        repeatregion = overlapsAny(vcf, repeatgff),
+                        ingene = overlapsAny(vcf, genes(gff)),
+                        coding = overlapsAny(vcf, coding),
+                        ref = as.character(rowRanges(vcf)$REF),
+                        alt = as.character(unlist(rowRanges(vcf)$ALT))
+                        )
 
+codingdf <- tibble(chr = as.character(seqnames(coding)),
+                   pos = start(coding),
+                   consequence = coding$CONSEQUENCE,
+                   refaa = as.character(coding$REFAA),
+                   varaa = as.character(coding$VARAA))
+
+locsdf <- tibble(chr = as.character(seqnames(locs)),
+                 pos = start(locs),
+                 locs = as.character(locs$LOCATION))
+
+locsdf <- locsdf %>%
+  unique() %>%
+  nest(locs, .key=locs) %>%
+  mutate(locs = map(.$locs,unlist)) %>%
+  mutate(locs = unlist(map(.$locs, str_c, collapse = ",")))
+
+#join the relevant data into one table and write
 variant_table <- variant_table %>%
-  left_join(refalt, by=c("chr","pos"))
+  left_join(locsdf, on = c("chr","pos")) %>%
+  left_join(codingdf, on=c("chr","pos")) %>%
+  mutate(upstreamkb = unlist(map2(.$chr,.$pos,~as.character(getSeq(fa,GRanges(seqnames = .x, IRanges(start = .y - 1000, end = .y - 1))))))) %>%
+  bind_cols(gts) %>%
+  mutate(downstreamkb = unlist(map2(.$chr,.$pos, ~as.character(getSeq(fa,GRanges(seqnames = .x, IRanges(start = .y + 1 , end = .y + 1000)))))))
 
 write_tsv(variant_table,"variants.tsv")
