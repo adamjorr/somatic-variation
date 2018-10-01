c7933ecb6f4fe7f55662c730931766346aa800ac
Adam Orr <adamjorr@gmail.com>
2018-10-01 15:41:32 -0700

    add script to plot variant locations on chromosomes

---
+++ plot_chromosomes.R
@@ -0,0 +1,22 @@
+#!/usr/bin/env Rscript
+library(VariantAnnotation)
+library(IRanges)
+library(GenomicRanges)
+library(GenomicFeatures)
+library(rtracklayer)
+
+args <- commandArgs(trailingOnly = TRUE)
+vcffile <- args[1]
+fastafile <- args[2]
+
+vcf <- readVcf(vcffile,fastafile)
+fa <- FaFile(fastafile)
+
+genome <- head(scanFaIndex(fa), n = 11)
+chrs <- as.character(seqnames(vcf))
+pos <- start(vcf)
+
+pdf("chromosome_plot.pdf",10,8)
+kp <- plotKaryotype(genome = genome, cex=.75)
+kpPoints(kp, chr=chrs, x=pos, y=.2)
+dev.off()
