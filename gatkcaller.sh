915e953353566363420028dbbe157a4dbbe29743
Adam Orr <adamjorr@gmail.com>
2018-10-29 14:01:09 -0700

    add updated versions of scripts

--- gatkcaller.sh
+++ gatkcaller.sh
@@ -1,15 +1,13 @@
 #!/usr/bin/bash
-##TODO: REMOVE ALIGNMENT COMPONENTS, UPDATE DOCS + VARIABLES
-#aligntoref.sh reference.fasta data.bam
-#Takes reference and aligns all .fastq files in any subdirectory and calls SNPs with GATK.
-#requires samtools, picard-tools, the GATK, and bowtie2
 
 #http://tldp.org/LDP/abs/html/string-manipulation.html is a great guide for manipulating strings in bash
 USAGE="Usage: $0 [-t THREADS] [-p PICARD_CMD] [-d TMPDIR] [-g  GATK_PATH] [-b BEDFILE] [-o OUTFILE] -r reference.fasta -i data.bam"
 
+#entry point for this script is a deduplified bam.
+
 #Here are some things you might want to change:
 PICARD="picard" #How do I call picard on this system?
-GATK="gatk" #How do I call GATK on this system?
+GATK=gatk #Location of your GATK jar
 CORES=48
 TMPOPTION=""
 OUTFILE=/dev/stdout
@@ -17,7 +15,6 @@ NUMNS=30
 BEDFILE=""
 REFERENCEFILE=""
 FILEIN=""
-HET="0.025"
 trap "exit 1" ERR
 
 while getopts :t:p:g:d:b:o:r:i:h opt; do
@@ -75,17 +72,15 @@ if [ "$REFERENCEFILE" == "" ]; then
 	exit 1
 fi
 
-if [ "$FILEIN" == "" ]; then
+if [ "FILEIN" == "" ]; then
 	echo $USAGE >&2
 	echo "Input file required." >&2
 	exit 1
 fi
 
-if [ "$TMPOPTION" != "" ]; then
-	mkdir -p $TMPOPTION #ensure temp directory exists if set
-fi
 
-TMPDIR=$(mktemp -d --tmpdir=$TMPOPTION gatkcaller_tmp_XXXXXX)
+#TMPDIR=$(mktemp -d --tmpdir=$TMPOPTION gatkcaller_tmp_XXXXXX)
+TMPDIR=./tmp/gatkcaller_tmp_QWSlQ4/
 # trap "rm -rf $TMPDIR" EXIT INT TERM HUP
 
 if [ "$BEDFILE" != "" ]; then
@@ -94,26 +89,23 @@ fi
 
 
 ###Below uses GATK to do some analysis.
-DEDUPLIFIEDBAM=$(mktemp --tmpdir=$TMPDIR --suffix=.bam dedup_XXX)
-METRICFILE=$(mktemp --tmpdir=$TMPDIR --suffix=.txt metrics_XXX)
+DEDUPLIFIEDBAM=${FILEIN}
 REFERENCEDICT=${REFERENCEFILE%.*}.dict
-FULLINTERVALS=$(mktemp --tmpdir=$TMPDIR --suffix=.interval_list fullIntervals_XXX)
-SCATTEREDINTERVALDIR=$(mktemp -d --tmpdir=$TMPDIR scatteredIntervals_XXXXXX)
-SCATTEREDFIRSTCALLDIR=$(mktemp -d --tmpdir=$TMPDIR scattered_first_calls_XXX)
+#FULLINTERVALS=$(mktemp --tmpdir=$TMPDIR --suffix=.interval_list fullIntervals_XXX)
+SCATTEREDINTERVALDIR=${TMPDIR}/scatteredIntervals_zkmc5a
+SCATTEREDFIRSTCALLDIR=${TMPDIR}/scattered_first_calls_Qvk
 SUFFIXES=$(seq -f %02.0f 0 $((CORES-1)))
+#SCATTEREDFIRSTCALLS=$(find $SCATTEREDFIRSTCALLDIR -name 'first_calls_*')
 SCATTEREDFIRSTCALLS=$(echo $SUFFIXES | tr ' ' '\n' | xargs -n 1 -i mktemp --tmpdir=$SCATTEREDFIRSTCALLDIR --suffix=.vcf.gz first_calls_{}_XXXXXX)
 CMDFIRSTCALLS=$(echo $SCATTEREDFIRSTCALLS | tr ' ' '\n' | xargs -i echo I={})
-JOINEDFIRSTCALLS=$(mktemp --tmpdir=$TMPDIR --suffix=.vcf joined_first_calls_XXX)
-SORTEDFIRSTCALLS=$(mktemp --tmpdir=$TMPDIR --suffix=.vcf sorted_first_calls_XXX)
-RECALIBRATEDBAM=$(mktemp --tmpdir=$TMPDIR --suffix=.bam recal_XXX)
-SCATTEREDOUTCALLDIR=$(mktemp -d --tmpdir=$TMPDIR scattered_output_calls_XXX)
-SCATTEREDOUTCALLS=$(echo $SUFFIXES | tr ' ' '\n' | xargs -n 1 -i mktemp --tmpdir=$SCATTEREDOUTCALLDIR --suffix=.vcf.gz out_call_{}_XXXXXX)
+JOINEDFIRSTCALLS=$TMPDIR/joined_first_calls_Fbl.vcf
+SORTEDFIRSTCALLS=$TMPDIR/sorted_first_calls_RuA.vcf
+RECALIBRATEDBAM=$TMPDIR/recal_Hrg.bam
+SCATTEREDOUTCALLDIR=$TMPDIR/scattered_output_calls_5Zv
+SCATTEREDOUTCALLS=$(find $SCATTEREDOUTCALLDIR -name 'out_call_*')
 CMDOUTCALLS=$(echo $SCATTEREDOUTCALLS | tr ' ' '\n' | xargs -i echo I={})
-OUTCALLS=$(mktemp --tmpdir=$TMPDIR --suffix=.vcf out_calls_XXX)
-RECALDATATABLE=$(mktemp --tmpdir=$TMPDIR --suffix=.table recal_data_XXX)
-
-
-$PICARD MarkDuplicates INPUT=$FILEIN OUTPUT=$DEDUPLIFIEDBAM METRICS_FILE=$METRICFILE MAX_FILE_HANDLES_FOR_READ_ENDS_MAP=1000
+OUTCALLS=$TMPDIR/out_calls_6ch.vcf
+RECALDATATABLE=$TMPDIR/recal_data_fzJ.table
 
 if [ ! -e ${REFERENCEFILE}.fai ]; then
 	samtools faidx $REFERENCEFILE
@@ -123,22 +115,23 @@ if [ ! -e ${REFERENCEDICT} ]; then
 	$PICARD CreateSequenceDictionary REFERENCE=${REFERENCEFILE} OUTPUT=${REFERENCEDICT}
 fi
 
-$PICARD BuildBamIndex INPUT=${DEDUPLIFIEDBAM}
+#$PICARD BuildBamIndex INPUT=${DEDUPLIFIEDBAM}
 
 #GATK TO RECALIBRATE QUAL SCORES + CALL VARIANTS
-gatk SplitIntervals -R ${REFERENCEFILE} --scatter-count $CORES -L scaffold_1  -L scaffold_2 -L scaffold_3 -L scaffold_4 -L scaffold_5 -L scaffold_6 -L scaffold_7 -L scaffold_8 -L scaffold_9 -L scaffold_10 -L scaffold_11 -O ${SCATTEREDINTERVALDIR}
-SCATTEREDINTERVALS=$(find ${SCATTEREDINTERVALDIR} -name '*.interval_list')
-parallel --halt 2 ${GATK} HaplotypeCaller --heterozygosity ${HET} -R ${REFERENCEFILE} -I $DEDUPLIFIEDBAM -L {1} ${BEDFILE} -stand_call_conf 50 -ploidy 2 -o {2} ::: $SCATTEREDINTERVALS :::+ $SCATTEREDFIRSTCALLS
-
-$PICARD SortVcf ${CMDFIRSTCALLS} O=${SORTEDFIRSTCALLS} SEQUENCE_DICTIONARY=${REFERENCEDICT}
-rm $SCATTEREDFIRSTCALLS ${SORTEDFIRSTCALLS}.idx
-
-${GATK} BaseRecalibrator -nct $CORES -I $DEDUPLIFIEDBAM -R ${REFERENCEFILE} ${BEDFILE} --knownSites $SORTEDFIRSTCALLS -o $RECALDATATABLE
-rm $SORTEDFIRSTCALLS
-${GATK} ApplyBQSR -I $DEDUPLIFIEDBAM -R ${REFERENCEFILE} --bqsr-recal-file ${RECALDATATABLE} -O $RECALIBRATEDBAM
-rm $DEDUPLIFIEDBAM $RECALDATATABLE
-parallel --halt 2 ${GATK} HaplotypeCaller -R ${REFERENCEFILE} -I $RECALIBRATEDBAM -L {1} ${BEDFILE} --heterozygosity 0.025 -ploidy 2 -o {2} ::: $SCATTEREDINTERVALS :::+ $SCATTEREDOUTCALLS
-rm $SCATTEREDINTERVALS $RECALIBRATEDBAM
+#$PICARD ScatterIntervalsByNs R=${REFERENCEFILE} OT=ACGT MAX_TO_MERGE=${NUMNS} O=${FULLINTERVALS}
+#$PICARD IntervalListTools I=${FULLINTERVALS} SCATTER_COUNT=$CORES O=${SCATTEREDINTERVALDIR}
+#gatk SplitIntervals -R ${REFERENCEFILE} --scatter-count $CORES -L scaffold_1 -L scaffold_2 -L scaffold_3 -L scaffold_4 -L scaffold_5 -L scaffold_6 -L scaffold_7 -L scaffold_8 -L scaffold_9 -L scaffold_10 -L scaffold_11 -O ${SCATTEREDINTERVALDIR}
+SCATTEREDINTERVALS=$(find ${SCATTEREDINTERVALDIR} -name '*.intervals')
+#the last one failed but let's continue on anyway. output = scattered 31, intervals = interval48
+#parallel --halt 2 ${GATK} HaplotypeCaller --heterozygosity 0.025 -R ${REFERENCEFILE} -I $DEDUPLIFIEDBAM ${BEDFILE} -stand-call-conf 50 -ploidy 2 -L {1} -O {2} ::: ${SCATTEREDINTERVALS} :::+ ${SCATTEREDFIRSTCALLS}
+#$PICARD SortVcf ${CMDFIRSTCALLS} O=${SORTEDFIRSTCALLS} SEQUENCE_DICTIONARY=${REFERENCEDICT}
+#rm $SCATTEREDFIRSTCALLS ${SORTEDFIRSTCALLS}.idx
+#${GATK} BaseRecalibrator -I $DEDUPLIFIEDBAM -R ${REFERENCEFILE} ${BEDFILE} --known-sites $SORTEDFIRSTCALLS -O $RECALDATATABLE
+#rm $SORTEDFIRSTCALLS
+${GATK} ApplyBQSR -I $DEDUPLIFIEDBAM -R ${REFERENCEFILE} --bqsr-recal-file $RECALDATATABLE -O $RECALIBRATEDBAM
+#rm $RECALDATATABLE
+parallel --halt 2 ${GATK} HaplotypeCaller -R ${REFERENCEFILE} -I $RECALIBRATEDBAM ${BEDFILE} --heterozygosity 0.025 -ploidy 2 -L {1} -O {2} ::: $SCATTEREDINTERVALS :::+ $SCATTEREDOUTCALLS
+#rm $SCATTEREDINTERVALS $RECALIBRATEDBAM
 $PICARD SortVcf ${CMDOUTCALLS} O=${OUTFILE} SEQUENCE_DICTIONARY=${REFERENCEDICT}
 
 exit 0
---
+++ optimize_parameters.R
@@ -0,0 +1,118 @@
+#!/usr/bin/Rscript --vanilla
+#
+# Copyright (c) 2017-2018 Reed A. Cartwright
+# Copyright (c) 2018      Adam J. Orr
+#
+# Authors:  Reed A. Cartwright <reed@cartwrig.ht>
+#           Adam J. Orr <adamjorr@gmail.com>
+#
+# This file is part of DeNovoGear.
+#
+# DeNovoGear is free software: you can redistribute it and/or modify it under
+# the terms of the GNU General Public License as published by the Free Software
+# Foundation; either version 3 of the License, or (at your option) any later
+# version.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along with
+# this program. If not, see <http://www.gnu.org/licenses/>.
+#
+
+options(stringsAsFactors=FALSE)
+
+library(doParallel)
+library(dfoptim)
+
+registerDoParallel()
+
+fixed_pars = c(
+    "mu" = 0,
+    "lib-bias" = 1
+)
+
+init_pars = c(
+    "theta" = 0.0260832395,
+    "lib-error" = 0.0009318686,
+    "ref-bias-hom" = 0.9567227229,
+    "ref-bias-het" = 0.8558397591,
+    "lib-overdisp-hom" = 0.0069877850,
+    "lib-overdisp-het" = 0.3060504752
+)
+
+lower_pars = c(
+    "mu" = 0,
+    "mu-library" = 0,
+    "mu-somatic" = 0,   
+    "theta" = 0,
+    "ref-bias-hom" = -1,
+    "ref-bias-het" = -1,
+    "ref-bias-hap" = -1,
+    "lib-bias" = 0,
+    "lib-error" = 0,
+    "lib-overdisp-hom" = 0,
+    "lib-overdisp-het" = 0  
+)
+
+upper_pars = c(
+    "mu" = 1,
+    "mu-library" = 1,
+    "mu-somatic" = 1,
+    "theta" = 1,
+    "ref-bias-hom" = 1,
+    "ref-bias-het" = 1,
+    "ref-bias-hap" = 1,
+    "lib-bias" = 2,
+    "lib-error" = 1,
+    "lib-overdisp-hom" = 1,
+    "lib-overdisp-het" = 1
+)
+
+# Run dng loglike and extract the log like
+run_once = function(cmd, pars) {
+    scanned_args = scan(text=cmd,what=character(),quiet=TRUE)
+    prog = scanned_args[1]
+    scanned_args = scanned_args[-1]
+    pars = c(fixed_pars, pars)
+    args = paste("--", names(pars), "=", pars,sep="")
+    o = which(scanned_args == "--")[1]
+    if(is.na(o)) {
+        args = c(scanned_args,args)
+    } else {
+        args = append(scanned_args,args,after=o-1)
+    }
+    out = system2(prog, args=args,stdout=TRUE,stderr=FALSE)
+    if(is.numeric(out)) {
+        return(NA)
+    }
+    score = strsplit(tail(out,3),"\t")[[1]][2]
+    -as.numeric(score)
+}
+
+loglike = function(pars,cmds,names) {
+    names(pars) = names
+    results = foreach(cmd=cmds,.combine=c) %dopar% run_once(cmd,pars)
+    total = sum(results)
+    total
+}
+
+main = function(cmds) {
+    pars = init_pars
+    par_names=names(pars)
+    o = nmkb(pars,loglike,lower=lower_pars[par_names],upper=upper_pars[par_names],
+         cmds=cmds,names=par_names,control=list(
+         trace=TRUE,tol=1e-6
+    ))
+    names(o$par) = par_names
+    o
+}
+
+if(!interactive()) {
+    ARGS = commandArgs(trailingOnly=TRUE)
+    cmds = readLines(ARGS[1])
+    o = main(cmds)
+    print(o)
+}
+
